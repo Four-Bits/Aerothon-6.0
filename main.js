e406b0c07271bf13c16ab44879e0d2f060f00d20
Harsh Valaki <96899883+TheLetterIsH@users.noreply.github.com>
2024-05-23 12:38:50 +0530

    🥶 Added mobile warning

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ menuToggle.click();
 
 menuToggle.addEventListener("click", (e) =>{
     if(!menuToggle.checked){
-        navbarContent.style.animation = "nav-in 300ms ease-in 0ms forwards";
+        navbarContent.style.animation = "nav-in 150ms ease-in-out 0ms forwards";
     }else{
 
-        navbarContent.style.animation = "nav-out 300ms ease-in 0ms forwards";
+        navbarContent.style.animation = "nav-out 150ms ease-in-out 0ms forwards";
     }
 })
